28636b080d511b00feeb25ae8d9cb569fc0299b1
kamalshree <codesqills@gmail.com>
2018-11-03 19:00:45 -0500

    Implemented the Navigation Bottom Bar

--- app/src/main/java/codesqills/org/techspeakup/ui/home/HomeActivity.java
+++ app/src/main/java/codesqills/org/techspeakup/ui/home/HomeActivity.java
@@ -1,7 +1,18 @@
 package codesqills.org.techspeakup.ui.home;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import codesqills.org.techspeakup.R;
 
@@ -10,12 +21,22 @@ import codesqills.org.techspeakup.R;
  */
 
 public class HomeActivity extends AppCompatActivity{
+    private BottomNavigationViewEx bnve;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_home_speaker);
+        bnve = (BottomNavigationViewEx) findViewById(R.id.navigation_home_speaker);
+
+        bnve.enableAnimation(true);
+        bnve.enableShiftingMode(false);
+        bnve.enableItemShiftingMode(false);
+
+
+
     }
 
 }
